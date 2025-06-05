ac9fbef0023cae0baa78ddfa325404b5bf603be3
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-04 22:26:53 -0300

    fix: changed the name of mealRoutes to mealsRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify';
 import { usersRoutes } from './http/usersRoutes';
 import fastifyCookie from '@fastify/cookie';
-import { mealsRoutes } from './http/mealRoutes';
+import { mealsRoutes } from './http/mealsRoutes';
 export const app = fastify();
 app.register(fastifyCookie);
 app.register(usersRoutes, {
