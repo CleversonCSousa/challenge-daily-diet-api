cc333474bb17b203badb5c0101fb20ca58378fe5
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-06 12:33:38 -0300

    feat:add authentication route

--- src/http/usersRoutes.ts
+++ src/http/usersRoutes.ts
@@ -1,5 +1,5 @@
 import { prismaClient } from '@/lib/prisma/prismaClient';
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { sign } from 'jsonwebtoken';
@@ -50,5 +50,51 @@ export async function usersRoutes(app: FastifyInstance) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     }).status(201).send();
+  });
+
+  app.post('/auth', async (request, reply) => {
+    const signInBodySchema = z.object({
+      email: z.string().email({
+        message: 'Invalid email'
+      }),
+      password: z.string()
+    });
+
+    const { email, password } = signInBodySchema.parse(request.body);
+
+
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email: email,
+      }
+    });
+
+    if (!user) {
+      return reply.status(401).send({
+        message: 'Incorrect credentials'
+      });
+    }
+
+    const passwordsMatch = await compare(password, user.password);
+
+    if (!passwordsMatch) {
+      return reply.status(401).send({
+        message: 'Incorrect credentials'
+      });
+    }
+
+    const token = sign({
+      name: user.name,
+      email: user.email,
+      id: user.id
+    }, env.JWT_SECRET);
+
+    return reply.cookie('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    }).status(200).send({
+      message: 'Authentication successful'
+    });
+
   });
 }
